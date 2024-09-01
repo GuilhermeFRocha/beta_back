beeb87ef99f8949e03d253e9d01535b74782fe39
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2024-09-01 15:42:24 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import Fastify from "fastify";
 const sequelize = require("./config/database");
 import userRoutes from "./routes/userRoutes";
+import cors from "@fastify/cors";
 
 const fastify = Fastify({
-  logger: true,
+  logger: {
+    level: "error",
+  },
+});
+
+fastify.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 });
 
 fastify.register(userRoutes);
@@ -11,8 +20,8 @@ fastify.register(userRoutes);
 const start = async () => {
   try {
     await sequelize.sync({ alter: true });
-    await fastify.listen({ port: 3000 });
-    console.log("Server running on http://localhost:3000");
+    await fastify.listen({ port: 8000 });
+    console.log("Server running on http://localhost:8000");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
