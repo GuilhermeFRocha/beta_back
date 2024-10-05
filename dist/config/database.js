013dc2a5107b1c7232866199d56c5d88e1d6418b
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2024-10-05 14:03:08 -0300

    add pg

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,24 +1,58 @@
 "use strict";
+// import { Sequelize } from "sequelize";
+// require("dotenv").config();
+// import pg from "pg";
+// const isProduction = process.env.NODE_ENV === "production";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const sequelize = new Sequelize(
+//   process.env.DATABASE || "verceldb",
+//   process.env.USER || "default",
+//   process.env.PASSWORD,
+//   {
+//     host: process.env.HOST,
+//     dialect: "postgres",
+//     dialectModule: pg,
+//     logging: false,
+//     dialectOptions: isProduction
+//       ? {
+//           ssl: {
+//             require: true, // Neon requer SSL em produção
+//             rejectUnauthorized: false, // Necessário para evitar erros de SSL
+//           },
+//         }
+//       : {},
+//   }
+// );
+// module.exports = sequelize;
 const sequelize_1 = require("sequelize");
-require("dotenv").config();
+const dotenv_1 = __importDefault(require("dotenv"));
 const pg_1 = __importDefault(require("pg"));
+dotenv_1.default.config();
 const isProduction = process.env.NODE_ENV === "production";
-const sequelize = new sequelize_1.Sequelize(process.env.DATABASE || "verceldb", process.env.USER || "default", process.env.PASSWORD, {
-    host: process.env.HOST,
+const sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URL ||
+    "postgres://default:wKL26tCWMUOV@localhost:5432/verceldb", {
     dialect: "postgres",
     dialectModule: pg_1.default,
     logging: false,
     dialectOptions: isProduction
         ? {
             ssl: {
-                require: true, // Neon requer SSL em produção
+                require: true, // Requer SSL em produção
                 rejectUnauthorized: false, // Necessário para evitar erros de SSL
             },
         }
         : {},
 });
-module.exports = sequelize;
+// Testar a conexão com o banco de dados
+sequelize
+    .authenticate()
+    .then(() => {
+    console.log("Conexão com o banco de dados foi bem-sucedida.");
+})
+    .catch((err) => {
+    console.error("Não foi possível conectar ao banco de dados:", err);
+});
+exports.default = sequelize;
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,49 +1,21 @@
-// import { Sequelize } from "sequelize";
-// require("dotenv").config();
-// import pg from "pg";
-// const isProduction = process.env.NODE_ENV === "production";
-
-// const sequelize = new Sequelize(
-//   process.env.DATABASE || "verceldb",
-//   process.env.USER || "default",
-//   process.env.PASSWORD,
-//   {
-//     host: process.env.HOST,
-//     dialect: "postgres",
-//     dialectModule: pg,
-//     logging: false,
-//     dialectOptions: isProduction
-//       ? {
-//           ssl: {
-//             require: true, // Neon requer SSL em produção
-//             rejectUnauthorized: false, // Necessário para evitar erros de SSL
-//           },
-//         }
-//       : {},
-//   }
-// );
-
-// module.exports = sequelize;
-
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+require("dotenv").config();
 import pg from "pg";
-
-dotenv.config();
-
 const isProduction = process.env.NODE_ENV === "production";
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL ||
-    "postgres://default:wKL26tCWMUOV@localhost:5432/verceldb",
+  process.env.DATABASE || "verceldb",
+  process.env.USER || "default",
+  process.env.PASSWORD,
   {
+    host: process.env.HOST,
     dialect: "postgres",
     dialectModule: pg,
     logging: false,
     dialectOptions: isProduction
       ? {
           ssl: {
-            require: true, // Requer SSL em produção
+            require: true, // Neon requer SSL em produção
             rejectUnauthorized: false, // Necessário para evitar erros de SSL
           },
         }
@@ -51,14 +23,4 @@ const sequelize = new Sequelize(
   }
 );
 
-// Testar a conexão com o banco de dados
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Conexão com o banco de dados foi bem-sucedida.");
-  })
-  .catch((err) => {
-    console.error("Não foi possível conectar ao banco de dados:", err);
-  });
-
-export default sequelize;
+module.exports = sequelize;
