f7c7f0a65706768e214cfd6de909b9cf323e34f6
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2024-10-05 12:45:32 -0300

    variables

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,11 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE || "verceldb",
-  process.env.POSTGRES_USER || "default",
-  process.env.POSTGRES_PASSWORD,
+  process.env.DATABASE || "verceldb",
+  process.env.USER || "default",
+  process.env.PASSWORD,
   {
-    host: process.env.POSTGRES_HOST,
+    host: process.env.HOST,
     dialect: "postgres",
     logging: false,
     dialectOptions: isProduction
